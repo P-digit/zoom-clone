13f7ca388fb68a05693378b2f0c7fbbb85119f8c
Park Jin soo <dndpr24@naver.com>
2022-08-02 23:14:13 +0900

    WebSocket setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import * as url from "url";
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000);
+const handleListen = () => console.log("Listening on http://localhost:3000");
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+server.listen(3000, handleListen);
